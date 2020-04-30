6f52bbdfdb651ec7d9e72ecf5002d5ece24dbfec
Matt Davis <mbdavis@deloitte.co.uk>
2020-04-30 20:19:19 +0100

    Test CI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const marqueeStyle = {
+  backgroundColor: 'black',
+  color: 'yellow',
+  fontSize: '100vh',
+  lineHeight: '100vh',
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <marquee scrollamount="100" style={marqueeStyle}>
+      SOS
+    </marquee>
   );
 }
 
